7620108dd6333146509be6d7c899bb1988100563
Lucas Honorio <lucashonorioo@hotmail.com>
2026-01-22 17:42:06 -0300

    insertion test in the productController

--- src/test/java/com/estudo/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/estudo/dscommerce/controllers/ProductControllerRA.java
@@ -1,7 +1,15 @@
 package com.estudo.dscommerce.controllers;
 
+import com.estudo.dscommerce.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -9,16 +17,45 @@ import static org.hamcrest.Matchers.*;
 
 public class ProductControllerRA {
 
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private String clientToken, adminToken, invalidToken;
     private long existingProductId, nonExistingProductId;
     private String nameProduct;
+
+    private Map<String, Object> postProductInstance;
 
     @BeforeEach
     public void setUp(){
         baseURI = "http://localhost:8080";
 
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto";
+
         existingProductId = 2L;
         nonExistingProductId = 100L;
         nameProduct = "PC Gamer";
+
+        postProductInstance = new HashMap<>();
+        postProductInstance.put("name", "Tablet");
+        postProductInstance.put("description", "tablet muito ruim");
+        postProductInstance.put("price", 50.0);
+        postProductInstance.put("imgUrl", "http://imagem.com");
+
+        List<Map<String, Object>> categories = new ArrayList<>();
+
+        Map<String, Object> category1 = new HashMap<>();
+        category1.put("id", 2);
+
+        categories.add(category1);
+
+        postProductInstance.put("categories", categories);
 
     }
 
@@ -68,5 +105,25 @@ public class ProductControllerRA {
 
     }
 
+    @Test
+    public void insertShouldReturnProductCreatedWhenAdminLogged(){
+
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(newProduct)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Tablet"))
+                .body("price", is(50.0F))
+                .body("categories.id", hasItems(2));
+
+    }
 
 }
