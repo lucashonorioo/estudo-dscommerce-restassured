2210d6d2337131d121e6251e80aa465caf8d8bb5
Lucas Honorio <lucashonorioo@hotmail.com>
2026-01-22 09:54:02 -0300

    Project created

---
+++ src/test/java/com/estudo/dscommerce/DscommerceRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceRestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
